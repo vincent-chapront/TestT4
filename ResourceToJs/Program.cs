f7f883809fc056d72668711c539763ed006ba9d6
Vincent Chapront <vincent.chapront@gmail.com>
2020-04-27 13:08:10 +0200

    Add project files.

---
+++ ResourceToJs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceToJs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResourceManager MyResourceClass = new ResourceManager(typeof(ResourceToJs.Properties.Resources /* Reference to your resources class -- may be named differently in your case */));
+
+            ResourceSet resourceSet = MyResourceClass.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+            foreach (DictionaryEntry entry in resourceSet)
+            {
+                string resourceKey = entry.Key.ToString();
+                object resource = entry.Value;
+            }
+        }
+    }
+}
---
+++ ResourceToJs/T4test.js
@@ -0,0 +1,13 @@
+﻿var Resources = {
+    'cc' : 'wx',
+    'aa' : 'azz',
+    'dd' : 'poi',
+    'bb' : 'qs',
+};
+var Resources_fr_FR = {
+    'e' : 'rt',
+    'cc' : 'xw',
+    'aa' : 'za',
+    'dd' : 'ze',
+    'bb' : 'sq',
+};
